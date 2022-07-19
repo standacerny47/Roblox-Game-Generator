a221f002969be5bea2f10bfafe25643ee2342a13
standdouble1 <89739704+standacerny47@users.noreply.github.com>
2022-07-19 16:08:55 +0200

    Add files via upload
    Needs Python to work.

---
+++ ROBLOX Game Generator.py
@@ -0,0 +1,11 @@
+import random, string
+import sys, os, time
+print('Roblox Game Generator by standdouble1')
+letters = int(input('Amountofnumbers: '))
+value = 1
+while value <= letters:
+ games = "https://roblox.com/games/" + ('').join(
+  random.choices(string.digits, k=letters))
+ f = open('Generatedgames.txt', "a+")
+ f.write(f'{games}\n')
+ print(f'[Generated!] {games}/')
